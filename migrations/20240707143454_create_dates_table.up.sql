d09a1694b588c2bc06e11e5e8a42568e9458fa5f
Aj-vrod <ammyvrodriguez@hotmail.com>
2024-08-06 20:32:39 +0200

    update migration files

--- migrations/20240707143454_create_dates_table.up.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS Dates(
-    date_id serial PRIMARY KEY,
-    day VARCHAR (20) NOT NULL
-);
--- migrations/20240707205142_create_news_table.up.sql
+++ migrations/20240707205142_create_news_table.up.sql
@@ -1,10 +1,7 @@
 CREATE TABLE IF NOT EXISTS News(
 	news_id serial PRIMARY KEY,
-	title VARCHAR(50) NOT NULL,
-	link VARCHAR(50) NOT NULL,
-	source VARCHAR(50) NOT NULL,
+	title VARCHAR(200) NOT NULL,
+	link VARCHAR(100) NOT NULL,
+	source VARCHAR(100) NOT NULL,
 	creation_date VARCHAR(20) NOT NULL,
-	date_id INT NOT NULL,
-	FOREIGN KEY (date_id)
-  	REFERENCES Dates(date_id)
 );
