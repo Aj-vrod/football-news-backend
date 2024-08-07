acb0a4f28d311ba6b394e62274229878506c0e30
Aj-vrod <ammyvrodriguez@hotmail.com>
2024-08-07 22:41:31 +0200

    create sql executer funcs

--- pkg/db/executor.go
+++ pkg/db/executor.go
@@ -6,15 +6,32 @@ import (
 	"github.com/football-news-backend/pkg/models"
 )
 
-// https://regex101.com/r/QgSa5i/1
 const (
-	getQuery    = "SELECT title, link, source, creation_date, date_id FROM news WHERE creation_date = $1;"
-	createQuery = "INSERT INTO news(title, link, source, creation_date) VALUES ($1, $2, $3, $4);"
+	getNewsQuery      = "SELECT title, link, source, creation_date FROM news;"
+	getDatedNewsQuery = "SELECT title, link, source, creation_date FROM news WHERE creation_date = $1;"
+	createNewsQuery   = "INSERT INTO news(title, link, source, creation_date) VALUES ($1, $2, $3, $4);"
 )
 
-func (d Database) GetNews(date string) ([]models.NewsItem, error) {
+func (d Database) GetDatedNews(date string) ([]models.NewsItem, error) {
 	news := []models.NewsItem{}
-	rows, err := d.DB.Query(getQuery, date)
+	rows, err := d.DB.Query(getDatedNewsQuery, date)
+	if err != nil {
+		log.Println("Failed to get dated news from database.")
+		return news, err
+	}
+
+	for rows.Next() {
+		var n models.NewsItem
+		rows.Scan(&n.Title, &n.URL, &n.Source, &n.Creation)
+		news = append(news, n)
+	}
+
+	return news, nil
+}
+
+func (d Database) GetNews() ([]models.NewsItem, error) {
+	news := []models.NewsItem{}
+	rows, err := d.DB.Query(getNewsQuery)
 	if err != nil {
 		log.Println("Failed to get news from database.")
 		return news, err
@@ -30,7 +47,7 @@ func (d Database) GetNews(date string) ([]models.NewsItem, error) {
 }
 
 func (d Database) CreateNews(newsItem models.NewsItem) error {
-	_, err := d.DB.Exec(createQuery, newsItem.Title, newsItem.URL, newsItem.Source, newsItem.Creation)
+	_, err := d.DB.Exec(createNewsQuery, newsItem.Title, newsItem.URL, newsItem.Source, newsItem.Creation)
 	if err != nil {
 		log.Println("Failed to create news item in database.")
 		return err
