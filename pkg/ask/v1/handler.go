b3b55c9cc6fd8bff2acd9315b5449302af8d5ee8
Aj-vrod <ammyvrodriguez@hotmail.com>
2024-08-07 22:22:58 +0200

    create validation for query input

--- pkg/ask/v1/handler.go
+++ pkg/ask/v1/handler.go
@@ -3,6 +3,7 @@ package askv1
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -16,14 +17,14 @@ type AskV1Response struct {
 func AskV1Handler(w http.ResponseWriter, req *http.Request) {
 	gemini := initGemini()
 	values := req.URL.Query()
-	query := values["query"][0]
-
-	if query == "" {
+	queries := values["query"]
+	if len(queries) <= 0 {
+		fmt.Fprint(w, "Missing query in request")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	ans := gemini.GenerateAnswer(query)
+	ans := gemini.GenerateAnswer(queries[0])
 	out := AskV1Response{
 		Answer: ans,
 	}
